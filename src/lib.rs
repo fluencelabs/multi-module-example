2f187ca4c1e2015f078e08a3dea346d999a726bc
vms <michail.vms@gmail.com>
2020-04-29 23:24:06 +0300

    fix pissble overflow

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use fluence::sdk::*;
 
-const REDIS_OPTION_KEYWORD: &str = "redis";
-const SQLITE_OPTION_KEYWORD: &str = "sqlite";
+const REDIS_OPTION_KEYWORD: &str = "redis ";
+const SQLITE_OPTION_KEYWORD: &str = "sqlite ";
 
 fn init() {
     logger::WasmLogger::init_with_level(log::Level::Info).unwrap();
@@ -11,10 +11,10 @@ fn init() {
 fn run(arg: String) -> Vec<u8> {
     if arg.starts_with(REDIS_OPTION_KEYWORD) {
         log::info!("calling redis");
-        redis::call(arg[REDIS_OPTION_KEYWORD.len() + 1..].as_bytes())
+        redis::call(arg[REDIS_OPTION_KEYWORD.len()..].as_bytes())
     } else if arg.starts_with(SQLITE_OPTION_KEYWORD) {
         log::info!("calling sqlite");
-        redis::call(arg[SQLITE_OPTION_KEYWORD.len() + 1..].as_bytes())
+        redis::call(arg[SQLITE_OPTION_KEYWORD.len()..].as_bytes())
     } else {
         Vec::from("unknown command")
     }
